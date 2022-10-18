69b350b11a0917f74b9d429cae6bead18779073f
beauwilliams <beau.williams@icloud.com>
2022-10-18 11:15:42 +1100

    fiw: clarify mock

--- contracts/mocks/ERC721WithRoyalties.mock.sol
+++ contracts/mocks/ERC721WithRoyalties.mock.sol
@@ -7,9 +7,14 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MockERC721WithRoyalties is ERC721, ERC2981 {
     constructor() ERC721("AfriMock", "AFRI") {
-        // set royalty of all NFTs to 5%
-        //default basis points is 10000 (100%)
-        //500/10000 = 5%
+        /*
+        @dev:
+        royalty of all NFTs is set to 5%
+        default basis points is 10000 (100%)
+        500/10000 = 5%
+        we can also set per token royalties
+        this mock does not cover per token royalties
+        */
         _setDefaultRoyalty(_msgSender(), 500);
     }
 
